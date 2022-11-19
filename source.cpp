75be41ed57e3cb648a1b319cfc334123e6500e77
_vanchieeu <chieuvanbn@gmail.com>
2022-11-19 22:20:19 +0700

    success

--- source.cpp
+++ source.cpp
@@ -1,93 +1,28 @@
 #include <iostream>
+#include <map>
 using namespace std;
 
-#define HASH_SIZE 1201
-
-typedef struct arr {
-    long n;
-
-    struct arr *next;
-} arr;
-arr *head[HASH_SIZE];
-
-void initHash(arr *head[]) {
-    for (int i = 0; i < HASH_SIZE; i++)
-        head[i] = NULL;
-}
-
-arr *creatArr(long n) {
-    arr *a = new arr;
-
-    a->n = n;
-    a->next = NULL;
-
-    return a;
-}
-
-int hash_func(long n) {
-    return n % HASH_SIZE;
-}
-
-void insertHash(long n) {
-    arr *a = new arr;
-    a = creatArr(n);
-
-    int index = hash_func(n);
-    if (head[index] == NULL) {
-        head[index] = a;
-    } else {
-        a->next = head[index];
-        head[index] = a;
-    }
-}
-
-void checkArr(long *a, bool *check, int n, long x) {
-    for (int i = 0; i < n; i++)
-        if (a[i] == x)
-            check[i] = false;
-}
-
-int countHash(long n) {
-    int index = hash_func(n);
-
-    arr *ptr = new arr;
-    ptr = head[index];
-
-    int count = 0;
-    while (ptr != NULL) {
-        count++;
-        ptr = ptr->next;
-    }
-
-    return count;
-}
-
 int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     int n;
     cin >> n;
 
-    initHash(head);
+    long long a[n];
+    map <long long, long long> b;
 
-    long a[n];
-    bool check[n];
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-
-        check[i] = true;
-        insertHash(a[i]);
+        b[a[i]]++;
     }
 
-    int count = 0;
-    for (int i = 0; i < HASH_SIZE; i++)
-        if (head[i] != NULL)
-            count++;
-    cout << count << endl;
-
+    cout << b.size() << endl;
     for (int i = 0; i < n; i++) {
-        if (check[i]) {
-            checkArr(a, check, n, a[i]);
-
-            cout << a[i] << " " << countHash(a[i]) << endl;
+        if (b[a[i]] > 0) {
+            cout << a[i] << " " << b[a[i]] << endl;
+            b[a[i]] = 0;
         }
     }
     return 0;
